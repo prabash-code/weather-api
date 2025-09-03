1cc908c287ee36f543559b9790e34cf2489e7ce2
Prabash Withanachchi <prabashwithanachchi11111@gmail.com>
2025-09-03 23:46:28 +0530

    commit

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const key = "19058284957e46e6aa135017252408";
+
+
+
+
 function clickAction() {
     let country = document.getElementById("txt-text-search").value;
 
@@ -8,16 +12,26 @@ function clickAction() {
     let temp = document.getElementById("temp_of_country");
     let image = document.getElementById("climate_image");
     let update = document.getElementById("last_update");
+    let windSpeed=document.getElementById("wind");
+    let windPressure=document.getElementById("pressure");
+    let humid=document.getElementById("humidity");
+    let windDr=document.getElementById("wind-dr");
+   
 
     fetch(`http://api.weatherapi.com/v1/current.json?key=19058284957e46e6aa135017252408&q=${country}`)
         .then(res => res.json())
         .then(data => {
             console.log(data);
-            let last_update = console.log(data.current.last_updated);
-            temp.innerHTML = data.current.temp_c + " C";
+         
+            temp.innerHTML = `${data.current.temp_c}\u2103`;
             name.innerHTML = data.location.name;
             climate.innerHTML = data.current.condition.text;
             image.innerHTML = `<img src="${data.current.condition.icon}" alt="">`
+            windPressure.innerHTML=`${data.current.pressure_mb} mb`;
+            windSpeed.innerHTML=`${data.current.wind_mph} mph`;
+            humid.innerHTML=`${data.current.humidity}  g/m^3`;
+            windDr.innerHTML=`${data.current.wind_dir}`;
+           // sunRice.innerHTML=data.forecast.forecastday[0].astro.sunrise;
 
 
             update.innerHTML = data.current.last_update;
@@ -55,6 +69,8 @@ function clickAction() {
     let typeHo7 = document.getElementById("ho7_type");
     let timeHo7 = document.getElementById("ho7_time");
 
+     let sunRice=document.getElementById("sunrise");
+
     fetch(`http://api.weatherapi.com/v1/forecast.json?key=19058284957e46e6aa135017252408&q=${country}`)
         .then(res => res.json())
         .then(element => {
@@ -64,29 +80,31 @@ function clickAction() {
             typeHo1.innerHTML = element.forecast.forecastday[0].hour[0].condition.text;
             photoHo1.innerHTML = `<img src="${element.forecast.forecastday[0].hour[0].condition.icon}" alt="Weather Icon">`
 
-            timeHo2.innerHTML = element.forecast.forecastday[0].hour[1].time;
-            typeHo2.innerHTML = element.forecast.forecastday[0].hour[1].condition.text;
-            photoHo2.innerHTML = `<img src="${element.forecast.forecastday[0].hour[1].condition.icon}" alt="Weather Icon">`
+            timeHo2.innerHTML = element.forecast.forecastday[0].hour[3].time;
+            typeHo2.innerHTML = element.forecast.forecastday[0].hour[3].condition.text;
+            photoHo2.innerHTML = `<img src="${element.forecast.forecastday[0].hour[3].condition.icon}" alt="Weather Icon">`
 
-            timeHo3.innerHTML = element.forecast.forecastday[0].hour[2].time;
-            typeHo3.innerHTML = element.forecast.forecastday[0].hour[2].condition.text;
-            photoHo3.innerHTML = `<img src="${element.forecast.forecastday[0].hour[2].condition.icon}" alt="Weather Icon">`
+            timeHo3.innerHTML = element.forecast.forecastday[0].hour[6].time;
+            typeHo3.innerHTML = element.forecast.forecastday[0].hour[6].condition.text;
+            photoHo3.innerHTML = `<img src="${element.forecast.forecastday[0].hour[6].condition.icon}" alt="Weather Icon">`
 
-            timeHo4.innerHTML = element.forecast.forecastday[0].hour[3].time;
-            typeHo4.innerHTML = element.forecast.forecastday[0].hour[3].condition.text;
-            photoHo4.innerHTML = `<img src="${element.forecast.forecastday[0].hour[3].condition.icon}" alt="Weather Icon">`
+            timeHo4.innerHTML = element.forecast.forecastday[0].hour[9].time;
+            typeHo4.innerHTML = element.forecast.forecastday[0].hour[9].condition.text;
+            photoHo4.innerHTML = `<img src="${element.forecast.forecastday[0].hour[9].condition.icon}" alt="Weather Icon">`
 
-            timeHo5.innerHTML = element.forecast.forecastday[0].hour[4].time;
-            typeHo5.innerHTML = element.forecast.forecastday[0].hour[4].condition.text;
-            photoHo5.innerHTML = `<img src="${element.forecast.forecastday[0].hour[4].condition.icon}" alt="Weather Icon">`
+            timeHo5.innerHTML = element.forecast.forecastday[0].hour[12].time;
+            typeHo5.innerHTML = element.forecast.forecastday[0].hour[12].condition.text;
+            photoHo5.innerHTML = `<img src="${element.forecast.forecastday[0].hour[12].condition.icon}" alt="Weather Icon">`
 
-            timeHo6.innerHTML = element.forecast.forecastday[0].hour[5].time;
-            typeHo6.innerHTML = element.forecast.forecastday[0].hour[5].condition.text;
-            photoHo6.innerHTML = `<img src="${element.forecast.forecastday[0].hour[5].condition.icon}" alt="Weather Icon">`
+            timeHo6.innerHTML = element.forecast.forecastday[0].hour[15].time;
+            typeHo6.innerHTML = element.forecast.forecastday[0].hour[15].condition.text;
+            photoHo6.innerHTML = `<img src="${element.forecast.forecastday[0].hour[15].condition.icon}" alt="Weather Icon">`
 
-            timeHo7.innerHTML = element.forecast.forecastday[0].hour[6].time;
-            typeHo7.innerHTML = element.forecast.forecastday[0].hour[6].condition.text;
-            photoHo7.innerHTML = `<img src="${element.forecast.forecastday[0].hour[6].condition.icon}" alt="Weather Icon">`
+            timeHo7.innerHTML = element.forecast.forecastday[0].hour[21].time;
+            typeHo7.innerHTML = element.forecast.forecastday[0].hour[21].condition.text;
+            photoHo7.innerHTML = `<img src="${element.forecast.forecastday[0].hour[21].condition.icon}" alt="Weather Icon">`
+
+            sunRice.innerHTML=element.forecast.forecastday[0].astro.sunrise;
 
         });
 
@@ -96,8 +114,9 @@ function clickAction() {
         .then(element1 => {
             let body = `<tr>
                 <th>Date   </th>
+                <th/>
                 <th>Climate</th>
-                <th></th>
+                
                 <th>WindSpeed(Kmph)</th>
                 <th>Humidity</th>
                 </tr> `
@@ -106,8 +125,9 @@ function clickAction() {
         body += `<tr>
                   
                     <td>${element1.forecast.forecastday[i].date}</td>
-                    <td>${element1.forecast.forecastday[i].hour[0].condition.text}</td>
+                    
                     <td><img src="${element1.forecast.forecastday[i].hour[0].condition.icon}" alt=""></td>
+                    <td>${element1.forecast.forecastday[i].hour[0].condition.text}</td>
                     <td>${element1.forecast.forecastday[i].hour[0].wind_kph}</td>
                     <td>${element1.forecast.forecastday[i].hour[0].humidity}</td>
                 </tr>`
